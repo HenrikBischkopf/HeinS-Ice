a5a11efe32bd38d239f5bafe76cc461c4c7de029
Henrik <henrik@bischkopf.de>
2024-10-27 23:45:05 +0100

    added links and a logo and some welcome text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header // </div>className="App-header"
+      >
+        <h1>Heins Ice</h1>
+        <img
+          src={`${process.env.PUBLIC_URL}/img/logoIceWaffle.svg`}
+          //className="App-logo"
+          alt="logo"
+          height={200}
+          width={200}
+        />
+        <p className="Attribution">Designed by Freepick.</p>
+        <p>Welcome to Heins Ice.</p>
+        <p>Is it Ice or Waffle season?</p>
+        <div className="linkBox">
+          <a href="/link1">Ice Season</a>
+          <a href="/link2">Waffle Season</a>
+        </div>
       </header>
     </div>
   );
